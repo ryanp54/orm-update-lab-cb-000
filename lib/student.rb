6cab7ff1860fbad53a41c15ea0636d26aa8d6f85
5-4 <github email address>
2017-12-14 12:33:17 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,69 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+	attr_accessor :name, :grade
+	attr_reader :id
 
+	def initialize(name, grade, id=nil)
+		@name = name
+		@grade = grade
+		@id = id
+	end
+
+	def save
+		if self.id
+			self.update
+		else
+			sql = <<-SQL
+			INSERT INTO students (name, grade)
+			VALUES (?, ?)
+			SQL
+			DB[:conn].execute(sql, [name, grade])
+			@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+			self
+		end
+	end
+
+	def update
+		sql = <<-SQL
+		UPDATE students
+		SET name = ?, grade = ?
+		WHERE id = ?
+		SQL
+		DB[:conn].execute(sql, name, grade, id)
+		self
+	end
+
+	def self.create_table
+		sql = <<-SQL
+		CREATE TABLE IF NOT EXISTS students (
+			id INTEGER PRIMARY KEY,
+			name TEXT,
+			grade INTEGER)
+		SQL
+		DB[:conn].execute(sql)
+	end
+
+	def self.drop_table
+		DB[:conn].execute("DROP TABLE IF EXISTS students")
+	end
+
+	def self.create(name, grade)
+		student = Student.new(name, grade)
+		student.save
+	end
+
+	def self.new_from_db(params)
+		Student.new(params[1], params[2], params[0])
+	end
+
+	def self.find_by_name(name)
+		sql = <<-SQL
+		SELECT * FROM students
+		WHERE name = ? LIMIT 1
+		SQL
+		params = DB[:conn].execute(sql, [name]).first
+		new_from_db(params)
+	end
 
 end
